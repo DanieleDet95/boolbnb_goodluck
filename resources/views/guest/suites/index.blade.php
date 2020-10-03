71fcfa6e606045d7f93f0ffed816dd12ae54ddb9
Ivan <ivan.monfrini@gmail.com>
2020-10-04 00:23:27 +0200

    Homepage quasi completata.
    manca barra di ricerca

--- resources/views/guest/suites/index.blade.php
+++ resources/views/guest/suites/index.blade.php
@@ -8,10 +8,10 @@
 
   {{-- Jumbotron --}}
   <section class="jumbotron jumbotron-fluid my_jumbotron py-0 mb-0">
-    <img src="{{asset('img/sfondo-jumbo-1081.jpg')}}" alt="">
+    <img src="{{asset('img/sfondo-jumbo-1082.jpg')}}" alt="">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-12 col-lg-5 col-md-8 title_jumbo">
+        <div class="col-12 col-lg-5 col-md-8 d-none d-lg-block d-xl-block title_jumbo">
           <h2>Riscopri l'Italia</h2>
           <p>Cambia quadro. Scopri alloggi nelle vicinanze tutti da vivere, per lavoro o svago.</p>
         </div>
@@ -25,45 +25,32 @@
   {{-- Suites cards --}}
   <section class="section_suites_cards">
     <div class="container">
-      <div class="row row-cols-md-3 p-3">
+      <div class="row row-cols-xs-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 p-3">
       {{-- Foreach suite with a promotion --}}
       @foreach ($highlights_suites_active as $highlight_suite_active)
-        <div class="container">
-          <a href={{ route("suites.show", $highlight_suite_active->id)}}>
-            <div class="example-row item p-3 m-3">
+        <a class="link_card" href={{ route("suites.show", $highlight_suite_active->id)}}>
+          <div class="example-row item">
+            <div class="example-content-main mx-3 polaroid">
+              {{-- cambio path della main_image a seconda dalla provenienza(faker o storage)  --}}
               {{-- Main Image --}}
-              <div class="example-content-main polaroid">
-                {{-- cambio path della main_image a seconda dalla provenienza(faker o storage)  --}}
-                @if (isset($highlight_suite_active->main_image))
-                  @if (strpos($highlight_suite_active->main_image, 'lorempixel') == false)
-                    <img src="{{ asset('storage') . "/" . $highlight_suite_active->main_image}}" alt="{{$highlight_suite_active->title}}">
-                   @else
-                   <img src="{{$highlight_suite_active->main_image}}" alt="{{$highlight_suite_active->title}}">
-                  @endif
+              @if (isset($highlight_suite_active->main_image))
+                @if (strpos($highlight_suite_active->main_image, 'lorempixel') == false)
+                  <img src="{{ asset('storage') . "/" . $highlight_suite_active->main_image}}" alt="{{$highlight_suite_active->title}}">
+                 @else
+                 <img src="{{$highlight_suite_active->main_image}}" alt="{{$highlight_suite_active->title}}">
                 @endif
-                <div class="example-content-secondary caption">
-                  <h5>{{$highlight_suite_active->title}}</h5>
-                  <p>{{$highlight_suite_active->address}}</p>
-                </div>
-              </div>
+              @endif
               {{-- End Main Image --}}
-
+              <div class="example-content-secondary caption">
+                <h5>{{$highlight_suite_active->title}}</h5>
+                <p>{{$highlight_suite_active->address}}</p>
+              </div>
             </div>
-          </a>
-        </div>
+          </div>
+        </a>
       @endforeach
       </div>
     </div>
   </section>
   {{-- end Suites cards --}}
-  {{-- PROVA --}}
-  <div class="item">
-    <div class="polaroid">
-      <img src="https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg">
-      <div class="caption">
-        I Miss London
-      </div>
-    </div>
-  </div>
-
 @endsection

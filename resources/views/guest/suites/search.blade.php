c2d964ad6cb0e7d64bf28e514a4b9055f14d649b
Novecento <ivan.monfrini@gmail.com>
2020-10-05 18:53:28 +0200

    Nuova search in arrivo

--- resources/views/guest/suites/search.blade.php
+++ resources/views/guest/suites/search.blade.php
@@ -3,35 +3,46 @@
 
 {{-- Yeld Main Content --}}
 @section("content")
-  <h1>Pagina ricerca</h1>
-
-
-  <div class="main-content">
-
-    {{-- è consigliato non utilizzare il tag form
-    per evitare che la pagina venga refreshata --}}
-    <div class="search-wrapper">
+  {{-- inizio pagina ricerca --}}
+  <div class="container">
+    <div class="row col-12">
       {{-- l'input search deve contenere l'id address-input
       per renderlo disponibile a places.js
       i data-att sono contengono le informazioni sulla latitudine e longitudine--}}
-      <input type="search" id="address-input" data-lat="" data-lng="{{old('searchbar')}}" placeholder="Where are we going?" />
+      <input class="form-control" type="search" id="address-input" data-lat="" data-lng="{{old('searchbar')}}" placeholder="Where are we going?">
+    </div>
 
-      <input id="range" type="number" name="range" value="" placeholder="Range in Km">
-      <input  id="rooms" type="number" name="rooms" value="" placeholder="Stanze">
-      <input  id="beds" type="number" name="beds" value="" placeholder="Letti">
-      <input  id="baths" type="number" name="baths" value="" placeholder="Bagni">
-      <input  id="square_m" type="number" name="square_m" value="" placeholder="Metri quadri">
+    <div class="row">
+      {{-- <div class="text-center"> --}}
+        <div class="col-2">
+          <input class="form-control" id="range" type="number" name="range" value="" placeholder="Distance in Km">
+        </div>
+        <div class="col-2">
+          <input class="form-control" id="rooms" type="number" name="rooms" value="" placeholder="Rooms">
+        </div>
+        <div class="col-2">
+          <input class="form-control" id="beds" type="number" name="beds" value="" placeholder="Beds">
+        </div>
+        <div class="col-2">
+          <input class="form-control" id="baths" type="number" name="baths" value="" placeholder="Baths">
+        </div>
+        <div class="col-2">
+          <input class="form-control" id="square_m" type="number" name="square_m" value="" placeholder="Square Meters">
+        </div>
+      {{-- </div> --}}
+      <div class="col-2">
+        {{-- <label>Price</label> --}}
+        <select class="custom-select" id="price" name="price">
+          <option value="0" selected="selected"> Price </option>
+          <option value="30"> < 30€ </option>
+          <option value="60"> < 60€ </option>
+          <option value="90"> < 90€ </option>
+          <option value="120"> < 120€ </option>
+        </select>
+      </div>
+    </div>
 
-      <label>Prezzo</label>
-      <select id="price" name="price">
-        <option value="0" selected="selected"> --- </option>
-        <option value="30"> < 30€ </option>
-        <option value="60"> < 60€ </option>
-        <option value="90"> < 90€ </option>
-        <option value="120"> < 120€ </option>
-      </select>
-
-
+    <div class="row form-check">
       <label>Piscina</label>
       <input id="pool" type="checkbox" name="pool" value="false">
       <label>wifi</label>
@@ -44,24 +55,28 @@
       <input id="piano" type="checkbox" name="piano" value="false">
       <label>Sauna</label>
       <input id="sauna" type="checkbox" name="sauna" value="false">
-
-
       {{-- non è importante il tipo di tag scelto ma deve contenere l'id #submit --}}
       <input id="submit" type="submit" value="Ricerca">
     </div>
 
+    {{-- è consigliato non utilizzare il tag form
+    per evitare che la pagina venga refreshata --}}
+    {{-- <div class="search-wrapper">
+    </div> --}}
+
     {{-- la classe del div serve ad identificare il punto di aggancio in cui riprodurre il template Handlebars
     se si rende necessario modificarlo, aggiornare il riferimento in search.js --}}
-    <div class="suites-cards">
-
+    <div class="row suites-cards">
     </div>
 
     {{-- l'id dello script serve ad identificare il template dalla funzione ajax in search.js
     se si rende necessario modificarlo, aggiornare il riferimento in search.js --}}
     <script id="suite-cards-template" type="text/x-handlebars-template">
-      <div class="entry">
-        <img src="@{{main_image}}" alt="@{{title}}">
-        <div class="body">
+      <div class="offset-2 col-3 entry d-flex flex-column">
+        <div>
+          <img src="@{{main_image}}" alt="@{{title}}">
+        </div>
+        <div class="body flex-grow-1">
           <h2>@{{title}}</h2>
           <h3>@{{address}}</h3>
           <h3>@{{price}}€</h3>

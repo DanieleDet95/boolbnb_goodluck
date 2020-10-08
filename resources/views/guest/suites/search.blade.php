6d3a122d7e4396578adc87041b09f36d55afb5dc
Novecento <ivan.monfrini@gmail.com>
2020-10-08 11:39:08 +0200

    quasi terminato

--- resources/views/guest/suites/search.blade.php
+++ resources/views/guest/suites/search.blade.php
@@ -90,7 +90,7 @@
     <div class="container-fluid container_suites_cards mb-5">
       <div class="row">
         <div class="col-lg-6 col-12 suites_cards">
-            <div class="suites_cards_promo">
+            <div class="suites_cards_promo bg-light">
               {{-- div per le card in evidenza --}}
             </div>
             <div class="suites_cards_noPromo">
@@ -106,19 +106,18 @@
     {{-- l'id dello script serve ad identificare il template dalla funzione ajax in search.js
     se si rende necessario modificarlo, aggiornare il riferimento in search.js --}}
     <script id="suite-cards-template" type="text/x-handlebars-template">
-        <div class="entry col-12 d-flex flex-column flex-sm-row">
+        <div class="entry py-3 col-12 d-flex align-items-center flex-column flex-sm-row">
           <div class="image_main_card col-12 col-sm-6">
             <img src="@{{main_image}}" class="d-block w-100" alt="@{{title}}">
           </div>
-          <div class="body_card col-12 col-sm-6">
-            <h4>@{{title}}</h4>
-            <h6>@{{address}}</h6>
+          <div class="body_card d-flex flex-column align-items-start mb-3 col-12 col-sm-6">
+            <h6>@{{title}}</h6>
+            <p>@{{address}}</p>
             <h6>@{{price}}€ / night</h6>
-            <p>@{{id}}</p>
           </div>
             <!-- <img class="image_main_cards" src="@{{main_image}}" alt="@{{title}}"> -->
         </div>
-      <hr>
+      <hr class="my-0">
     </script>
 
 @endsection

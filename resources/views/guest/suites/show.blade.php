32644b68d18163a2f6bad32cf2e571e6701e4125
Novecento <ivan.monfrini@gmail.com>
2020-10-09 16:47:03 +0200

    mo spacco tutto

--- resources/views/guest/suites/show.blade.php
+++ resources/views/guest/suites/show.blade.php
@@ -3,6 +3,7 @@
 
 {{-- Yeld Main Content --}}
 @section("content")
+  {{-- Container-fluid --}}
   <div class="container-fluid container-md">
     {{-- Row Title Images --}}
     <div class="row flex-column flex-nowrap mt-3 row_title_img">
@@ -48,132 +49,128 @@
       <div class="col-12 my_suite_description">
         <p>{{$suite->description}}</p>
       </div>
-      <hr>
+
       <div class="col-6 my_suite_info">
         <h4>Suite Info</h4>
-        <ul class="list-group">
+        <ul class="list_infos mb-0">
           <li>Rooms: {{$suite->rooms}}</li>
           <li>Beds: {{$suite->beds}}</li>
           <li>Baths: {{$suite->baths}}</li>
           <li>Square Meters: {{$suite->square_m}}</li>
-          <li>Price: {{$suite->price}}</li>
+          <li>Price: {{$suite->price}} €/ night</li>
         </ul>
       </div>
 
       <div class="col-6 my_suite_services">
         @if (!$suite->services->isEmpty())
           <h4>Suite Services</h4>
-          <ul class="list-group">
+          <ul class="list_services mb-0">
           @foreach ($suite->services as $service)
             <li><i class="{{ $service->icon }}"></i> {{ $service->supplements }}</li>
           @endforeach
           </ul>
         @endif
       </div>
+      <hr class="w-100">
     </div>
     {{-- Fine Row Details --}}
-  </div>
-  <div class="message_to_landowner">
-    <div class="col-12 my_landowner_info">
-      <h3>Write to owner
-        @if (!empty($suite->user->name))
-          {{$suite->user->name}}
-        @endif
-        @if (!empty($suite->user->lastname))
-          {{$suite->user->lastname}}
-        @endif
-      </h3>
-      <a href="#">{{$suite->user->email}}</a>
+    {{-- Row Message --}}
+    <div class="row row_message">
+      <div class="message_to_landowner">
+        <div class="col-12 my_landowner_info">
+          <h3>Write to owner
+            @if (!empty($suite->user->name))
+              {{$suite->user->name}}
+            @endif
+            @if (!empty($suite->user->lastname))
+              {{$suite->user->lastname}}
+            @endif
+          </h3>
+        </div>
+      </div>
     </div>
+    {{-- Fine Row Message --}}
+    {{-- Row Form --}}
+    <div class="row row_form">
+      @if (!is_null($user))
+
+        @if (!($suite->user_id === $user->id))
+
+          @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul class="list-group">
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+
+          {{-- <div> --}}
+            <form action="{{route("suites.store_message", $suite)}}" method="post">
+              @csrf
+              @method("POST")
+
+              <div class="mail">
+                <label></label>
+                <input type="email" name="email" value="{{ old("email") }}" placeholder="Enter your email">
+              </div>
+              <div class="name">
+                <label>Inserisci il nome</label>
+                <input type="text" name="name" value="{{ old("name") }}">
+              </div>
+              <div class="body">
+                <label>Inserisci Contenuto</label>
+                <textarea name="body" rows="8" cols="80">{{ old("body") }}</textarea>
+              </div>
+              <div class="mail">
+                <label>Inserisci Mail</label>
+                <input type="submit" value="submit">
+              </div>
+            </form>
+          {{-- </div> --}}
+
+        @endif
+
+      @else
+
+          @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+
+          <div>
+            <form action="{{route("suites.store_message", $suite)}}" method="post">
+              @csrf
+              @method("POST")
+
+              <div class="mail">
+                <label>Inserisci Mail</label>
+                <input type="email" name="email" value="{{ old("email") }}">
+              </div>
+              <div class="name">
+                <label>Inserisci il nome</label>
+                <input type="text" name="name" value="{{ old("name") }}">
+              </div>
+              <div class="body">
+                <label>Inserisci Contenuto</label>
+                <textarea name="body" rows="8" cols="80">{{ old("body") }}</textarea>
+              </div>
+              <div class="mail">
+                <label>Inserisci Mail</label>
+                <input type="submit" value="submit">
+              </div>
+            </form>
+          </div>
+
+      @endif
+    </div>
+    {{-- Fine Row Form --}}
   </div>
-  <ul>
-
-    {{-- <li>Latitudine: {{$suite->latitude}}</li>
-    <li>Longitudine: {{$suite->longitude}}</li>
-    <li>id: {{$suite->id}}</li> --}}
-
-
-  {{-- cambio path delle immagini della tabella images a seconda dalla provenienza(faker o storage)  --}}
-
-
-  {{-- l'admin può inviare un messaggio quando NON è proprietario dell'appartamento selezionato --}}
-
-  @if (!is_null($user))
-
-    @if (!($suite->user_id === $user->id))
-
-      @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul class="list-group">
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-
-      <div>
-        <form action="{{route("suites.store_message", $suite)}}" method="post">
-          @csrf
-          @method("POST")
-
-          <div class="mail">
-            <label>Inserisci Mail</label>
-            <input type="email" name="email" value="{{ old("email") }}">
-          </div>
-          <div class="name">
-            <label>Inserisci il nome</label>
-            <input type="text" name="name" value="{{ old("name") }}">
-          </div>
-          <div class="body">
-            <label>Inserisci Contenuto</label>
-            <textarea name="body" rows="8" cols="80">{{ old("body") }}</textarea>
-          </div>
-          <div class="mail">
-            <label>Inserisci Mail</label>
-            <input type="submit" value="submit">
-          </div>
-        </form>
-      </div>
-
-    @endif
-
-  @else
-
-      @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-
-      <div>
-        <form action="{{route("suites.store_message", $suite)}}" method="post">
-          @csrf
-          @method("POST")
-
-          <div class="mail">
-            <label>Inserisci Mail</label>
-            <input type="email" name="email" value="{{ old("email") }}">
-          </div>
-          <div class="name">
-            <label>Inserisci il nome</label>
-            <input type="text" name="name" value="{{ old("name") }}">
-          </div>
-          <div class="body">
-            <label>Inserisci Contenuto</label>
-            <textarea name="body" rows="8" cols="80">{{ old("body") }}</textarea>
-          </div>
-          <div class="mail">
-            <label>Inserisci Mail</label>
-            <input type="submit" value="submit">
-          </div>
-        </form>
-      </div>
-
-  @endif
-
+  {{-- Fine Container-fluid --}}
 @endsection
